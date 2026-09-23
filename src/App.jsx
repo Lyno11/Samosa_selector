d3baced6ed7b56c105ba756e79244d3b44d1b045
Lyno11 <tc92576n@pace.edu>
2026-09-23 19:51:12 -0400

    Step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const [multiplier, setMultiplier] = useState(1)
+
+  const updateCount = () => setCount(count + multiplier)
+
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
+        <h2>Count: {count}</h2>
         <img
           className="samosa"
           src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Samosachutney.jpg"
           alt="A plate of samosas"
+          onClick={updateCount}
         />
       </div>
     </div>
