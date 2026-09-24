e176895ab338ce68f3621923a53886a064baf364
Lyno11 <tc92576n@pace.edu>
2026-09-23 20:03:03 -0400

    Step4

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,24 @@ const App = () => {
 
   const updateCount = () => setCount(count + multiplier)
 
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2)
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5)
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier(multiplier * 10)
+    }
+  }
+
   return (
     <div className="App">
       <div className="header">
@@ -24,19 +42,19 @@ const App = () => {
         <div className="upgrade">
           <h3>Double Stuffed 👯‍♀️</h3>
           <p>2x per click</p>
-          <button>10 samosas</button>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
         </div>
 
         <div className="upgrade">
           <h3>Party Pack 🎉</h3>
           <p>5x per click</p>
-          <button>100 samosas</button>
+          <button onClick={buyPartyPack}>100 samosas</button>
         </div>
 
         <div className="upgrade">
           <h3>Full Feast 👩🏽‍🍳</h3>
           <p>10x per click</p>
-          <button>1000 samosas</button>
+          <button onClick={buyFullFeast}>1000 samosas</button>
         </div>
       </div>
     </div>
